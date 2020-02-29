7c6926a8fb8cccbc664397030099c931fcff9d69
Даниил Максимович Орел <264898@niuitmo.ru>
2020-02-29 15:45:54 +0300

    User login

---
+++ Ecobamboni/Controllers/AuthorizationController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Ecobamboni.Models;
+
+namespace Ecobamboni.Controllers
+{
+    public class AuthorizationController : ApiController
+    {
+        private UserContext db = new UserContext();
+
+        // GET: api/Authorization
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Authorization/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Authorization/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(string id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Login)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Authorization
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (db.Users.Find(user.Login) != null)
+            {
+                User dbuser = db.Users.Find(user.Login);
+                if (user.Password == dbuser.Password)
+                    return CreatedAtRoute("DefaultApi", dbuser.Id, dbuser);
+                else BadRequest();
+            }
+
+            return BadRequest();
+        }
+
+        // DELETE: api/Authorization/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(string id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(string id)
+        {
+            return db.Users.Count(e => e.Login == id) > 0;
+        }
+    }
+}
